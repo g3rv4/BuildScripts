ee9fbf306033fa725623da0f8bb6e323d1cdff55
Gervasio Marchand <github@gervas.io>
2022-01-14 22:10:07 -0300

    build the package!

---
+++ build.ps1
@@ -0,0 +1,24 @@
+$basePath = Join-Path (Get-Location) .build
+$publishPath = Join-Path $basePath publish
+$packagesPath = Join-Path $basePath packages
+
+if (Test-Path $basePath) {
+  Remove-Item $basePath -Recurse -Force
+}
+
+New-Item -Path $basePath -ItemType 'Container' | Out-Null
+New-Item -Path $packagesPath -ItemType 'Container' | Out-Null
+
+dotnet tool restore
+$version = (dotnet tool run nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
+
+Copy-Item ./BuildScripts $publishPath
+$nuspecPath = Join-Path $publishPath buildscripts.nuspec
+(Get-Content buildscripts.nuspec).Replace('$version$', $version) | Set-Content $nuspecPath
+
+$nupkgPath = Join-Path $packagesPath "buildscripts.$($version).nupkg"
+Compress-Archive -Path "$($publishPath)/*" -DestinationPath $nupkgPath
+
+if ($env:GITHUB_ENV) {
+    Write-Output "VERSION=$version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+}
