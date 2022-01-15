9f154527e8267debf38231ad0ca6fe63ee481e5d
Gervasio Marchand <github@gervas.io>
2022-01-15 19:09:39 -0300

    Add manifest

---
+++ BuildScripts/BuildScripts.psd1
@@ -0,0 +1,15 @@
+@{
+ModuleVersion = '0.0.1'
+
+GUID = '643906bc-fc44-4f6c-b41a-c9f7a9b89de0'
+Author = 'Gervasio Marchand Cassataro'
+CompanyName = 'Gervasio Marchand Cassataro'
+Copyright = '(c) Gervasio Marchand Cassataro. All rights reserved.'
+FunctionsToExport = @('Build-Package')
+CmdletsToExport = @()
+VariablesToExport = '*'
+AliasesToExport = @()
+
+DefaultCommandPrefix = 'BS'
+}
+
--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $basePath = Join-Path (Get-Location) .build
 $publishPath = Join-Path $basePath publish
+$manifestPath = Join-Path $publishPath BuildScripts.psd1
 $packagesPath = Join-Path $basePath packages
 
 if (Test-Path $basePath) {
@@ -12,9 +13,10 @@ New-Item -Path $packagesPath -ItemType 'Container' | Out-Null
 dotnet tool restore
 $version = (dotnet tool run nbgv get-version -f json | ConvertFrom-Json).SimpleVersion
 
-Copy-Item ./BuildScripts $publishPath
+Copy-Item BuildScripts $publishPath -Recurse
 $nuspecPath = Join-Path $publishPath buildscripts.nuspec
 (Get-Content buildscripts.nuspec).Replace('$version$', $version) | Set-Content $nuspecPath
+(Get-Content $manifestPath).Replace("ModuleVersion = '0.0.1'", "ModuleVersion = '$version'") | Set-Content $manifestPath
 
 $nupkgPath = Join-Path $packagesPath "buildscripts.$($version).nupkg"
 Compress-Archive -Path "$($publishPath)/*" -DestinationPath $nupkgPath
